ae24164c1de660c16cceef66698da75742bb1a2f
AdrianNegoita93 <44656326+AdrianNegoita93@users.noreply.github.com>
2018-11-01 01:41:55 +0200

    Add files via upload

---
+++ GCI2018_Practice_1.py
@@ -0,0 +1,18 @@
+i = 0                                           #I created the variable "i" to count how many times the message is displayed .
+while i < 10:
+	print(" GCI is great")
+	i += 1
+                                                #This is the end of display . 
+print("Hello! What's your name?")
+name = input()                                  #Now the user is expected to enter his name 
+print('Hello ' , name ,', please to meet you!')
+x=(name[::-1])                                  #Now the user's name is reversed and stored in the "x"
+print('Did you know that your name backwards is ' , x)
+
+
+                                                #And this is my first code in the Python programming language :)
+
+
+        
+
+
